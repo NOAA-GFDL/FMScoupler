abb420988c5541159e2a3f2a01f35ff938167383
rem1776 <Ryan.Mulhall@lscamd50-d.gfdl.noaa.gov>
2022-06-02 11:37:18 -0400

    path fixes

--- t/null_model_build.sh
+++ t/null_model_build.sh
@@ -25,8 +25,8 @@ mk_template=${bld_dir}/mkmf/templates/linux-ubuntu-xenial-gnu.mk
 
 # FMS
 if [ "$1" = "--link-fms" ]; then
-  test -f "${script_root}/../../FMS" || echo "Error: --link-fms specified but FMS src not found(${script_root}/../../FMS)"
-  ln -s $(readlink -f ../../FMS) $src_dir/FMS
+  test -d "${script_root}/../../FMS" || echo "Error: --link-fms specified but FMS src not found(${script_root}/../../FMS)"
+  ln -s $(readlink -f ../FMS) $src_dir/FMS
 else
   git clone https://github.com/NOAA-GFDL/FMS.git $src_dir/FMS
 fi
