59c663687295ec0dbe1d23b35e86a482555e931a
Zhihong Tan <Zhihong.Tan@gaea52.ncrc.gov>
2023-10-26 18:45:55 -0400

    MODIFIED SIMPLE COUPLER FOR NEW SURFACE FLUX
    The same code modifications as the full coupler are now incorporated
    into the simple coupler, which is now compatible with the updated
    surface_flux subroutine.

--- simple/flux_exchange.F90
+++ simple/flux_exchange.F90
@@ -59,6 +59,7 @@ integer :: id_drag_moist,  id_drag_heat,  id_drag_mom,              &
            id_u_star, id_b_star, id_q_star, id_u_flux, id_v_flux,   &
            id_t_surf, id_t_flux, id_q_flux, id_r_flux,              &
            id_t_atm,  id_u_atm,  id_v_atm,  id_wind,                &
+           id_thv_atm, id_thv_surf,                                 & ! ZNT
            id_t_ref,  id_rh_ref, id_u_ref,  id_v_ref,  id_q_ref,    &
            id_del_h,  id_del_m,  id_del_q, id_albedo,  id_gust,     &
            id_t_ca,   id_q_surf, id_q_atm, id_z_atm, id_p_atm,      &
@@ -152,7 +153,8 @@ real, dimension(is:ie,js:je) :: u_surf, v_surf, rough_heat, rough_moist, &
                                 albedo, albedo_vis_dir, albedo_nir_dir,  &
                                 albedo_vis_dif, albedo_nir_dif,          &
                                 del_m, del_h, del_q, land_frac,          &
-                                ref, ref2, t_ref, qs_ref, qs_ref_cmip
+                                ref, ref2, t_ref, qs_ref, qs_ref_cmip,   &
+                                thv_atm, thv_surf
 
 logical, dimension(is:ie,js:je) :: mask, seawater, avail
 real :: zrefm, zrefh
@@ -256,6 +258,7 @@ real :: zrefm, zrefh
                       flux_t, flux_q, flux_lw, flux_u, flux_v,           &
                       cd_m,   cd_t, cd_q, wind,                          &
                       u_star, b_star, q_star,                            &
+                      thv_atm, thv_surf,                                 &
                       dhdt_surf, dedt_surf, dedq_surf, drdt_surf,        &
                       dhdt_atm,  dedq_atm,   dtaudu_atm, dtaudv_atm,     &
                       dt, Land%mask(:,:,1), seawater, avail              )
@@ -282,6 +285,14 @@ real :: zrefm, zrefh
    Boundary%b_star    = b_star
    Boundary%q_star    = q_star
 
+ ! additional boundary variables for use in ncep-edmf
+   Boundary%shflx     = flux_t
+   Boundary%lhflx     = flux_q
+   Boundary%wind      = wind
+   Boundary%thv_atm   = thv_atm
+   Boundary%thv_surf  = thv_surf
+
+
 !=======================================================================
 !-------------------- diagnostics section ------------------------------
 
@@ -303,6 +314,8 @@ real :: zrefm, zrefh
    if ( id_u_star      > 0 ) used = fms_diag_send_data ( id_u_star,      u_star,       Time )
    if ( id_b_star      > 0 ) used = fms_diag_send_data ( id_b_star,      b_star,       Time )
    if ( id_q_star      > 0 ) used = fms_diag_send_data ( id_q_star,      q_star,       Time )
+   if ( id_thv_atm     > 0 ) used = fms_diag_send_data ( id_thv_atm,     thv_atm,      Time )
+   if ( id_thv_surf    > 0 ) used = fms_diag_send_data ( id_thv_surf,    thv_surf,     Time )
    if ( id_t_atm       > 0 ) used = fms_diag_send_data ( id_t_atm,       Atm%t_bot,    Time )
    if ( id_u_atm       > 0 ) used = fms_diag_send_data ( id_u_atm,       Atm%u_bot,    Time )
    if ( id_v_atm       > 0 ) used = fms_diag_send_data ( id_v_atm,       Atm%v_bot,    Time )
@@ -791,6 +804,9 @@ subroutine flux_up_to_atmos (Time, Land, Ice, Boundary )
         allocate( land_ice_atmos_boundary%shflx(is:ie,js:je) )!miz
         allocate( land_ice_atmos_boundary%lhflx(is:ie,js:je) )!miz
 #endif
+   allocate( land_ice_atmos_boundary%wind(is:ie,js:je) )    !ZNT
+   allocate( land_ice_atmos_boundary%thv_atm(is:ie,js:je) ) !ZNT
+   allocate( land_ice_atmos_boundary%thv_surf(is:ie,js:je) )!ZNT
    allocate( land_ice_atmos_boundary%rough_mom(is:ie,js:je) )
    allocate( land_ice_atmos_boundary%frac_open_sea(is:ie,js:je) )
 
@@ -818,6 +834,9 @@ subroutine flux_up_to_atmos (Time, Land, Ice, Boundary )
    land_ice_atmos_boundary%shflx=0.0
    land_ice_atmos_boundary%lhflx=0.0
 #endif
+   land_ice_atmos_boundary%wind=0.0     ! ZNT
+   land_ice_atmos_boundary%thv_atm=0.0  ! ZNT
+   land_ice_atmos_boundary%thv_surf=0.0 ! ZNT
    land_ice_atmos_boundary%rough_mom=0.01
    land_ice_atmos_boundary%frac_open_sea=0.0
 
@@ -935,9 +954,17 @@ subroutine diag_field_init ( Time, atmos_axes )
    fms_diag_register_diag_field ( mod_name, 'b_star',     atmos_axes, Time, &
                         'buoyancy scale',      'm/s2'   )
 
-  id_q_star     = &
-       fms_diag_register_diag_field ( mod_name, 'q_star',     atmos_axes, Time, &
+   id_q_star     = &
+   fms_diag_register_diag_field ( mod_name, 'q_star',     atmos_axes, Time, &
        'moisture scale',      'kg water/kg air'   )
+
+   id_thv_atm = &   ! ZNT
+   fms_diag_register_diag_field ( mod_name, 'thv_atm', atmos_axes, Time, &
+       'surface air virtual potential temperature', 'K')
+
+   id_thv_surf = &  ! ZNT
+   fms_diag_register_diag_field ( mod_name, 'thv_surf', atmos_axes, Time, &
+       'surface virtual potential temperature', 'K')
 
    id_u_flux     = &
    fms_diag_register_diag_field ( mod_name, 'tau_x',      atmos_axes, Time, &
@@ -1156,6 +1183,7 @@ subroutine surface_flux_2d (                                           &
      flux_t,    flux_q,     flux_r,    flux_u,    flux_v,              &
      cd_m,      cd_t,       cd_q,                                      &
      w_atm,     u_star,     b_star,     q_star,                        &
+     thv_atm,   thv_surf,                                              &  ! ZNT 05/03/2020
      dhdt_surf, dedt_surf,  dedq_surf,  drdt_surf,                     &
      dhdt_atm,  dedq_atm,   dtaudu_atm, dtaudv_atm,                    &
      dt,        land,       seawater,  avail  )
@@ -1173,6 +1201,7 @@ subroutine surface_flux_2d (                                           &
        dhdt_surf, dedt_surf,  dedq_surf, drdt_surf,          &
        dhdt_atm,  dedq_atm,   dtaudu_atm,dtaudv_atm,         &
        w_atm,     u_star,     b_star,    q_star,             &
+       thv_atm,   thv_surf,                                  &  ! ZNT 05/03/2020
        cd_m,      cd_t,       cd_q
   real, intent(inout), dimension(:,:) :: q_surf
   real, intent(in) :: dt
@@ -1189,6 +1218,7 @@ subroutine surface_flux_2d (                                           &
           flux_t(:,j),    flux_q(:,j),     flux_r(:,j),    flux_u(:,j),    flux_v(:,j),             &
           cd_m(:,j),      cd_t(:,j),       cd_q(:,j),                                               &
           w_atm(:,j),     u_star(:,j),     b_star(:,j),     q_star(:,j),                            &
+          thv_atm(:,j),   thv_surf(:,j),                                                            &  ! ZNT 05/03/2020
           dhdt_surf(:,j), dedt_surf(:,j),  dedq_surf(:,j),  drdt_surf(:,j),                         &
           dhdt_atm(:,j),  dedq_atm(:,j),   dtaudu_atm(:,j), dtaudv_atm(:,j),                        &
           dt,             land(:,j),       seawater(:,j),  avail(:,j)  )
